81d76d5286b5fe39ff251177ef9c90bd956fb6b5
baileytorch <baileytorch@hotmail.com>
2025-12-03 16:06:17 -0300

    Procesando datos usuarios api

--- negocio/negocio_users.py
+++ negocio/negocio_users.py
@@ -1,7 +1,18 @@
 import requests
+from prettytable import PrettyTable
 
 
 def obtener_usuarios_api(url):
+    tabla_usuarios = PrettyTable()
+    tabla_usuarios.field_names=['N°','Nombre','Nombre Usuario','Correo','Latitud']
     respuesta = requests.get(url)
     if respuesta.status_code == 200:
-        print(respuesta.text)
+        listado_usuarios = respuesta.json()
+        for usuario in listado_usuarios:
+            tabla_usuarios.add_row([
+                usuario['id'],
+                usuario['name'],
+                usuario['username'],
+                usuario['email'],
+                usuario['address']['geo']['lat']])
+    print(tabla_usuarios)
